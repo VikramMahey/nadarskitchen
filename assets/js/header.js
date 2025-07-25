b55376b40e9e24f6d372fa636d194a1513f9a85a
Vikram <vikram@ >
2025-07-25 23:27:59 +0530

    hero section updated

---
+++ assets/js/header.js
@@ -0,0 +1,9 @@
+export function initHeader() {
+const toggle = document.getElementById("nav-toggle");
+const nav = document.getElementById("main-nav");
+
+toggle.addEventListener("click", () => {
+  nav.classList.toggle("open");
+});
+
+}
---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+import { initHeader } from './header.js';
+// import { initHero } from './hero.js';
+// import { initMenu } from './menu.js';
+// import { initContact } from './contact.js';
+
+document.addEventListener("DOMContentLoaded", () => {
+  initHeader();
+//   initHero();
+//   initMenu();
+//   initContact();
+});
