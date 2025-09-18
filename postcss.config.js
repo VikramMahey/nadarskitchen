f8575e049bc6274a10af047bc64c77cad170a4e7
Vikram <vikram@ >
2025-09-19 02:47:58 +0530

    css updated

--- postcss.config.js
+++ postcss.config.js
@@ -5,7 +5,7 @@ module.exports = {
     plugins: [
         require('cssnano')({ preset: 'default' }), // minify CSS
         require('postcss-modules')({
-            generateScopedName: name => name, // keep original class names
+            generateScopedName: '[hash:base64:6]', // keep original class names
             getJSON: function (cssFileName, json, outputFileName) {
                 const jsonPath = path.resolve('./css/class-names.json');
                 fs.writeFileSync(jsonPath, JSON.stringify(json));
