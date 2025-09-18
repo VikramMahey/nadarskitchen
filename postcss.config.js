24f618d0e2bec3be27aa5cce7b265f95a59e6dee
Vikram <vikram@ >
2025-09-19 02:06:07 +0530

    css updated

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,7 @@
 module.exports = {
     plugins: [
-        require('postcss-modules')({
-            generateScopedName: '[hash:base64:5]', // short, unique class names
-            globalModulePaths: [/global\.css$/],   // optional: leave global CSS unchanged
-        })
-    ]
+        require('cssnano')({
+            preset: 'default',
+        }),
+    ],
 };
