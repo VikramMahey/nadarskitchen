10efd896bbb7a4614ac2186c1d80de34ac847ad6
Vikram <vikram@ >
2025-09-19 02:18:09 +0530

    css updated

--- postcss.config.js
+++ postcss.config.js
@@ -5,3 +5,19 @@ module.exports = {
         }),
     ],
 };
+
+
+module.exports = {
+    plugins: [
+        require('postcss-modules')({
+            generateScopedName: '[hash:base64:6]', // short, unreadable class names
+            getJSON: function (cssFileName, json, outputFileName) {
+                // Save mapping to a JSON file to replace class names in HTML/JS if needed
+                const fs = require('fs');
+                const path = require('path');
+                const jsonPath = path.resolve('./css/class-names.json');
+                fs.writeFileSync(jsonPath, JSON.stringify(json));
+            }
+        })
+    ]
+};
